da28b22324fb2931d829b8cf176ce0a5a7db7bda
Blue Staggo <owengrenney@gmail.com>
2024-06-01 11:35:10 +0100

    The tweakening

--- src/main/java/io/bluestaggo/authadvlite/mixin/player/PlayerEntityMixin.java
+++ src/main/java/io/bluestaggo/authadvlite/mixin/player/PlayerEntityMixin.java
@@ -4,15 +4,14 @@ import net.minecraft.entity.living.LivingEntity;
 import net.minecraft.entity.living.player.PlayerEntity;
 import net.minecraft.entity.player.HungerManager;
 import net.minecraft.entity.player.PlayerAbilities;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.command.source.CommandSource;
 import net.minecraft.world.World;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
@@ -20,11 +19,21 @@ public abstract class PlayerEntityMixin extends LivingEntity implements CommandS
 	@Shadow public PlayerAbilities abilities;
 	@Shadow protected HungerManager hungerManager;
 
-	@Shadow
-	public abstract boolean needsHealing();
+	@Shadow public abstract boolean needsHealing();
+
+	@Shadow private ItemStack itemInUse;
 
 	public PlayerEntityMixin(World world) {
 		super(world);
+	}
+
+	@ModifyConstant(
+		method = "tick",
+		constant = @Constant(intValue = 25)
+	)
+	private int longerEating(int value) {
+		if (this.itemInUse == null) return value;
+		return this.itemInUse.getUseDuration() - 7;
 	}
 
 	@Unique
@@ -52,25 +61,30 @@ public abstract class PlayerEntityMixin extends LivingEntity implements CommandS
 	@Redirect(
 		method = "tickNonRidingMovementRelatedStats",
 		at = @At(
+			value = "FIELD",
+			target = "Lnet/minecraft/entity/living/player/PlayerEntity;onGround:Z",
+			opcode = Opcodes.GETFIELD
+		)
+	)
+	private boolean applySprintExhaustionInAir(PlayerEntity instance) {
+		return this.onGround || this.isSprinting();
+	}
+
+	@Redirect(
+		method = "tickNonRidingMovementRelatedStats",
+		at = @At(
 			value = "INVOKE",
 			target = "Lnet/minecraft/entity/living/player/PlayerEntity;addFatigue(F)V",
 			ordinal = 2
 		)
 	)
 	private void exhaustionOnSprinting(PlayerEntity player, float amount) {
-		this.addCustomFatigue(amount * 2.0F);
-	}
+		amount *= 2.0F;
+		if (!this.onGround) {
+			amount *= 1.5F;
+		}
 
-	@Redirect(
-		method = "jump",
-		at = @At(
-			value = "INVOKE",
-			target = "Lnet/minecraft/entity/living/player/PlayerEntity;addFatigue(F)V",
-			ordinal = 0
-		)
-	)
-	private void exhaustionOnSprintJumping(PlayerEntity player, float amount) {
-		this.addCustomFatigue(1.0F);
+		this.addCustomFatigue(amount);
 	}
 
 	@ModifyVariable(
